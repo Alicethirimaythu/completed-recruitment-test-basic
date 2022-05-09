ca23eee227db90da6d3d19fa885dbd309a83bfcf
Thiri May Thu <thirimaythu@Thiris-MacBook-Pro.local>
2022-05-09 01:04:36 +0100

    Finished all the tasks

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,11 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
-export default function () {
 
-    async function getEmployees() {
-        return fetch("/employees").then(response => response.json());
+export default function () {
+    const[data, setData] = useState();
+    const [formData, setFormData] = useState({name: "", value: ""});
+    const [searchName, setSearchName] = useState("");
+    const [editValue, setEditValue] = useState({name: "", value: ""});
+
+    useEffect(() => {
+        getEmployees();
+    }, []);
+
+    const onChangeHandler = (name) => {
+        return e => setFormData({...formData, [name]: e.target.value});
     }
 
+    const onClickHandler = async () =>{
+            await createEmployee(formData.name, formData.value);
+            await getEmployees();
+            setFormData({name: "", value: ""});
+            //window.location.reload();
+    }
+
+    const onClickEdit = async () =>{
+        await updateEmployee(formData.name, formData.value);
+        await getEmployees();
+        setFormData({name: "", value: ""});
+    }
+
+    async function getEmployees() {
+        // return fetch("/employees").then(response => response.json());
+        const response = await fetch("/employees");
+        const data = await response.json();
+        setData(data);
+    }
+    
     async function createEmployee(name, value) {
         return fetch("/employees", {
             method: 'POST',
@@ -22,7 +51,41 @@ export default function () {
         });
     }
 
+    
     return (
-        <div>Complete your app here</div>
+        // <div>{JSON.stringify(data)}</div>
+        <div>
+            <h2>Employee List</h2>
+            <table style={{width: "50%"}}>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {data && data.map((dt, i)=>(
+                    <tr key={dt.name + i}>
+                        <td>{dt.name}</td>
+                        <td>{dt.value}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            
+            <p>
+            <h3>Add a new employee or edit the value of an employee</h3>
+                {formData.name}<br/>
+                <label>Employee Name: <input type="text" value={formData.name} onChange = {onChangeHandler("name")}></input></label>
+            </p>
+            <p>
+                {formData.value}<br/>
+                <label>Employee Value: <input type="text" value={formData.value} onChange = {onChangeHandler("value")}></input></label>
+            </p>
+            <button onClick={onClickHandler}>Create</button>
+            <button onClick={onClickEdit}>Edit Value</button>
+        </div>
+
     );
+
 }
